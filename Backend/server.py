c5a503981aba3a7be9234f82cf4f160e6b8afee9
Kaleb Demaline <kd148920@ohio.edu>
2024-09-02 13:36:30 -0400

    Add Options to backend

--- Backend/server.py
+++ Backend/server.py
@@ -11,6 +11,14 @@ class testHttp(BaseHTTPRequestHandler):
 
     handler = DataHandler()
 
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
     def do_GET(self):
         path = urlparse(self.path).path
 
